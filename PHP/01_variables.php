18cfba225771493dbb710adc1b85f8e608d8b7c4
Sweetpotato <67162997+sweetpotatoX@users.noreply.github.com>
2025-09-14 08:29:29 +0300

    Add PHP variable and string examples
    Introduced two new files: 01_variables.php demonstrates PHP variable types, printing, and type checking; 02_strings.php provides basic string manipulation examples. These files serve as introductory learning resources for PHP basics.

---
+++ PHP/01_variables.php
@@ -0,0 +1,77 @@
+<?php
+
+// Variables & types
+/*
+ * string
+ * integer
+ * boolean
+ * float or double
+ * null
+ * array
+ * object
+ * resources
+ */
+$name = 'John Doe';
+$nametwo = 'Sweetpotato';
+$age = 18;
+$isfather = true;
+$money = 11.22;
+$salary = null;
+
+
+// print Variables
+
+
+//echo "Hello" . $name . "<br>";
+//echo " I am {$nametwo} <br>";
+//echo "My age is" . $age;
+//echo "<br>";
+//echo $isfather;
+//echo "<br>";
+//echo $money;
+//echo "<br>";
+//echo $salary;
+//
+
+
+
+//What I have learnt
+/*
+ * when using the '' it will be all text
+ * when using the "" we can use the variables inside of it using {}
+ * when we try to add a variable or a text to the echo we use .
+ */
+
+
+// Types of Vars
+
+//echo gettype($name) . "<br>";
+//echo gettype($age) . "<br>";
+//echo gettype($isfather) . "<br>";
+//echo gettype($money) . "<br>";
+//echo gettype($salary) . "<br>";
+
+// Print the whole variable
+
+//print_r($name);
+//echo "<br>" ;
+//var_dump($name);
+//echo "<br>" ;
+
+//What I have learnt
+/*
+ * The space is counted in the string
+ */
+
+//change the value and the type of a var
+//$name = 32;
+//echo gettype($name);
+
+//variable checking functions
+//var_dump(is_integer($isfather) );
+
+
+// how to check if the Var is defined
+//var_dump(isset($name));
+//echo '<br>';
+//var_dump(isset($namethreee));
---
+++ PHP/02_strings.php
@@ -0,0 +1,6 @@
+<?php
+//$str = "WHAT THE FUCK IS THAT";
+//echo strtolower($str);
+//
+//echo str_replace("FUCK", "HELL", $str);
+//echo str_word_count($str);
